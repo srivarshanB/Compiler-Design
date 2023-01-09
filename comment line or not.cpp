de4021754de43abc664c6a3a01b31823bf0972b1
srivarshanB <112372900+srivarshanB@users.noreply.github.com>
2023-01-09 14:39:55 +0530

    Add files via upload

---
+++ comment line or not.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char com[30];int i=2,a=0;
+printf("\nEnter comment:");
+gets(com);if(com[0]=='/'){
+if(com[1]=='/')
+printf("\nIt is a comment");
+else if(com[1]=='*'){
+for(i=2;i<=30;i++)
+{
+if(com[i]=='*'&&com[i+1]=='/')
+{
+printf("\nIt is a comment");
+a=1;
+break;
+}
+else continue;
+}
+if(a==0)
+printf("\nIt is not a comment");
+}
+else
+printf("\nIt is not a comment");
+}
+else
+printf("\nIt is not a comment");
+}
+
